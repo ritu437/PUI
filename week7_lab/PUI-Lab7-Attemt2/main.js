2b4597ed1fa57458e4b671ec53959b0c0f184fea
ritu437 <ritu437@users.noreply.github.com>
2018-10-16 12:29:22 -0400

    Re-do of lab 7
    Starting over bc didn't really get to do it in lab.
    Created HTML, CSS, and JS file.
    Created constructors in main.js

---
+++ week7_lab/PUI-Lab7-Attemt2/main.js
@@ -0,0 +1,31 @@
+
+
+/*Constructors for zoo animals*/
+
+    function Fox(name, age){
+        this.name = name;
+        this.age = age;
+        this.type = "Fox"
+        this.img = "fox.jpg"
+    }
+
+    function Tiger(name, age){
+        this.name = name;
+        this.age = age;
+        this.type = "Siberian Tiger"
+        this.img = "tiger.jpg"
+    }
+
+    function Leopard(name, age){
+        this.name = name;
+        this.age = age;
+        this.type = "Snow Leopard"
+        this.img = "leopard.jpg"
+    }
+
+    function Lynx(name, age){
+        this.name = name;
+        this.age = age;
+        this.type = "Lynx"
+        this.img = "lynx.png"
+    }
