aa21ab33f7b16d40838071e9d7e24b347756d7f3
ritu437 <ritu437@users.noreply.github.com>
2018-11-17 16:09:57 -0500

    Lab 11 Code till step 7

---
+++ pui-templates-lab11-master/main.js
@@ -0,0 +1,54 @@
+// TODO (Step 2): fill in code creating products objects here
+var products = {
+    "cinnamonBuns": [
+        {
+            "name": "Birthday Cake",
+            "price": "$3.00",
+            "img": "img/birthday-cake.jpg",
+            "id": 0
+        },
+        {
+            "name": "Lavender",
+            "price": "$2.50",
+            "img": "img/lavender.png",
+            "id": 1
+        },
+        {
+            "name": "Maple Walnut",
+            "price": "$2.00",
+            "img": "img/maple-walnut.jpg",
+            "id": 2
+        }
+    ]
+}
+
+$(document).ready(function(){
+    // TODO (Step 4): fill in code for changing selection-template here
+    // get the HTML template using jQuery
+    var source = $("#selection-template").html();
+    // compile the template into a function
+    var template = Handlebars.compile(source);
+    // create new HTML using our data
+    var newHTML = template(products);
+    // add the new HTML to the page
+    $("#product-selection-container").append(newHTML);
+
+    // TODO (Step 7): add function call for 1st update to detail-template
+    updateProductDetail(2);
+
+    // TODO: (Step 8): add code below for changing product details on click
+
+
+});
+
+// TODO (Step 6): fill in function for updating detail-template based on id
+function updateProductDetail(id) {
+    // get the HTML template using jQuery
+    var source = $("#detail-template").html();
+    // compile the template into a function
+    var template = Handlebars.compile(source);
+    // create new HTML using our data
+    var newHTML = template(products["cinnamonBuns"][id]);
+    // add the new HTML to the page
+    $("#product-detail-container").append(newHTML);
+}
