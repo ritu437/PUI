8aa1cdac9594db0916a4f5d54f0cc2190197eeff
ritu437 <ritu437@users.noreply.github.com>
2018-11-28 21:06:06 -0500

    hover on
     when hover on, change to gif

--- assignment8/js/consentMain.js
+++ assignment8/js/consentMain.js
@@ -2,11 +2,25 @@
 /*on document ready*/
 $(document).ready(function(){
     console.log("ready");
-    $(".module-img img").click(function(){
+/*    $(".module-img img").click(function(){
         var imgSrc = $(this).attr("id");
         console.log("img " + imgSrc);
         var gifSrc = $(this).attr("gif-src");
         console.log("gif " + gifSrc);
+    });*/
+
+    $(".module-img img").hover(function(){
+        var curSrc = $(this).attr("src");
+        console.log("current src: " +curSrc);
+        var imgSrc = $(this).attr("id");
+        console.log("img " + imgSrc);
+        var gifSrc = $(this).attr("gif-src");
+        console.log("gif " + gifSrc);
+        $(this).attr("src", gifSrc);
+        var newSrc = $(this).attr("src");
+        console.log("new src: " +newSrc);
+    }, function(){
+
     });
 
 
